69abc38963c93e515a880d56f4511f9470a07fc9
sai16121046 <53078046+sai16121046@users.noreply.github.com>
2019-07-19 23:17:42 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -2,8 +2,8 @@
 ch=input('enter the character:')
 if ((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')):
     if (ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch== 'u' or ch=='U'):
-        print('vowel')
+        print('Vowel')
     else:
-        print('consonant')
+        print('Consonant')
 else:
-    print('invalid')
+    print('Invalid')
