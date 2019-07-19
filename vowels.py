844acdfb668599ecf0f336d1cf58903ffc11d897
sai16121046 <53078046+sai16121046@users.noreply.github.com>
2019-07-19 23:12:30 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,4 +1,4 @@
-#saikumar
+#saikumarreddy
 ch=input('enter the character:')
 if ((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')):
     if (ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch== 'u' or ch=='U'):
