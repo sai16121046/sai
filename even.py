2d34a1975fccf1ab6bc56be16e9bb555305ec67f
sai16121046 <53078046+sai16121046@users.noreply.github.com>
2019-07-19 15:35:26 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,9 +1,8 @@
-num = int(input("enter the number:"))
-if (num>0):
-      if(num % 2) == 0:
-            print("even")
-      else:
-            print("odd")
+num = int(input())
+if(num % 2) == 0:
+      print("even")
+elif(num % 2)!=0:
+      print("odd")
 else:
       print("invalid")
 
