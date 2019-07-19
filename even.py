76eef62ba4f1df2c0ff9dea2f810b221f726e0ed
sai16121046 <53078046+sai16121046@users.noreply.github.com>
2019-07-19 15:23:01 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,6 +1,9 @@
-num = int(input("enter the number:"))    
-if (num  % 2) == 0:
-      print("even")
+num = int(input("enter the number:"))
+if num > 0:
+      if(num % 2) == 0:
+            print("even")
+      else:
+            print("odd")
 else:
-      print("odd")
+            print("invalid")
 
