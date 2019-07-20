019423b2f2739613406eca644ea498fce3d2ebf5
sai16121046 <53078046+sai16121046@users.noreply.github.com>
2019-07-20 16:51:15 +0530

    Update largesh.py

--- largesh.py
+++ largesh.py
@@ -1,10 +1,8 @@
-num1 = float(input("enter first number:"))
-num2 = float(input("enter second number:))
-num3 = float(input("enter third number:))
+num1,num2,num3=map(int,input().split())
 if (num1 >= num2) and (num1 >= num3):
    largest = num1
 elif (num2 >= num1) and (num2 >= num3):
    largest = num2
 else:
    largest = num3
-print("The largest number between",num1,",",num2,"and",num3,"is",largest)
+print(largest)
